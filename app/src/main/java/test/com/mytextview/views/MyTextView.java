3f2b89a1deab42dbf3526fbdd2a871b2d8bd406b
shivam340 <shivam@leftshift.io>
2015-09-11 18:42:39 +0530

    overrided onDraw()

--- app/src/main/java/test/com/mytextview/views/MyTextView.java
+++ app/src/main/java/test/com/mytextview/views/MyTextView.java
@@ -1,6 +1,7 @@
 package test.com.mytextview.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -8,7 +9,7 @@ import android.view.View;
  * Created by shivam on 9/11/15.
  */
 public class MyTextView extends View {
-    
+
     public MyTextView(Context context) {
         super(context);
     }
@@ -16,4 +17,24 @@ public class MyTextView extends View {
     public MyTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //method to draw things on canvas (empty space given to you by system)
+
+        /*
+            # If you set a background drawable for a View, then the View will draw it for you before calling back to its onDraw() method.
+            # onAttachedToWindow () is called when the view is attached to a window. At this point it has a Surface and will start drawing.
+            Note that this function is guaranteed to be called before onDraw(android.graphics.Canvas), however it may be called any time before the first onDraw
+            -- including before or after onMeasure(int, int).
+
+            invalidate() mark the area defined by dirty as needing to be drawn. If the view is visible, onDraw(android.graphics.Canvas) will be called at some point in the future.
+        */
+
+
+    }
+
+
+
 }
