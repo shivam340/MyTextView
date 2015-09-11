e94bb9338642d8b8a5bfd38b553fc44e8d2d3a28
shivam340 <shivam@leftshift.io>
2015-09-11 18:27:28 +0530

    created custome view class and added constructors

---
+++ app/src/main/java/test/com/mytextview/views/MyTextView.java
@@ -0,0 +1,19 @@
+package test.com.mytextview.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by shivam on 9/11/15.
+ */
+public class MyTextView extends View {
+    
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
